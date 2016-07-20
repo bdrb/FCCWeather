e14f5f2724219fa55b0ca1b6cea24380c66fb1f9
bdrb <bdrb@ukr.net>
2016-07-20 07:02:13 +0300

    2

--- app.js
+++ app.js
@@ -8,10 +8,61 @@ var app = {};
 	    //$("#weather").load(app.apiUrlGet(coord));
 	    $.get(app.apiUrlGet(coord),function(data, status){
 	    	$("#weather").text(data);
+
+             /*
+      {"coord":{"lon":139,"lat":35},
+      "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
+      "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+      "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+      "wind":{"speed":7.31,"deg":187.002},
+      "rain":{"3h":0},
+      "clouds":{"all":92},
+      "dt":1369824698,
+      "id":1851632,
+      "name":"Shuzenji",
+      "cod":200}
+    */
+
+    $("#lat").text(data.coord.lat);
+    $("#lon").text(data.coord.lon);        
+    //<div id = "sys">
+    $("#country").text(data.sys.country);
+    $("#sunrise").text(data.sys.sunrise);
+    $("#sunset").text(data.sys.sunset);
+//        <div id = "weather">
+    $("#wid").text(data.weather[0].id);
+    $("#main").text(data.weather[0].main);
+    $("#description").text(data.weather[0].description);
+    $("#icon").text(data.weather[0].icon);
+    //    </div>
+    $("#temp").text(data.main.temp);
+    $("#humidity").text(data.main.humidity);
+    $("#pressure").text(data.main.pressure);
+    $("#temp_min").text(data.main.temp_min);
+    $("#temp_max").text(data.main.temp_max);
+    //    </div>
+    //    <div id = "wind">
+    $("#speed").text(data.wind.speed);
+    $("#deg").text(data.wind.deg);
+    //    </div>
+    //    <div id = "rain">
+    $("#3h").text(data.rain.3h);
+    //    </div>
+    //    <div id = "clouds">
+    $("#all").text(data.clouds.all);
+    //    </div>
+    $("#dt").text(data.dt);
+    $("#id").text(data.id);
+    $("#name").text(data.name);
+    $("#cod").text(data.cod);
+*/
+            
 	    })
     }
 
 $(document).ready(function(){
+
+       
 	app.weatherGet({"lon":139,"lat":35});
 });
 
