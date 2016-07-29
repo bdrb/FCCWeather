583796b5f19030323e87daaa236093dbf2f3ddff
gennady tabala <gennady.tabala@gmail.com>
2016-07-29 16:15:34 +0300

    it work.. but decr..

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.items.coord.lon = "City geo location, longitude";
 app.items.coord.lat = "City geo location, latitude";
 app.items.weather = {}; //(more info Weather condition codes)
 app.items.weather.id = "Weather condition id";
-app.items.weather.main  = "Group of weather parameters (Rain, Snow, Extreme etc.)";
+app.items.weather.main = "Group of weather parameters (Rain, Snow, Extreme etc.)";
 app.items.weather.description = "Weather condition within the group";
 app.items.weather.icon = "Weather icon id";
 app.items.base = "Internal parameter";
@@ -47,117 +47,55 @@ app.weatherGet = function (coord) {
     //$("#weather").load(app.apiUrlGet(coord));
     $.get(app.apiUrlGet(coord), function (data, status) {
 
-        function itemShow(dt, lvl)
-        {
-            for(var i = 0; i < Object.keys(dt).length; i++)
-            {
-                var key = Object.keys(dt)[i];
-                if(typeof dt[key] == "object")
-                {
-                    if(Array.isArray(dt[key]))
-                    {
-                        for(var ii = 0; ii < dt[key].length; ii++)
-                        {
-                            itemShow(dt[key][ii], lvl + "@#$" + key);
-                        }
-                    }
-                    else
-                    {
-                        itemShow(dt[key], lvl + "@#$" + key);
-                    }    
-                }
-                else
-                {
-                    //$("#" + key  + "").text(lvl + "," + key + ": " + dt[key]);
-
-                    var lvlArr = lvl.split("@#$");
-                    lvlArr.push(key);
-
-                    var lastElem = $("#" + lvlArr[0]);
-                    for (var i = 1; i < lvlArr.length; i++)
-                    {
-                       /* if(lastElem == "")
-                        {
-                            var elems = $("#" + lvlArr[i]);
-                        }
-                        else
-                        {*/
-                            var elems = $("#" + lastElem[0].id + ">" + lvlArr[i]);
-                       // }
-                        if (elems.length == 0){
-                            $(lastElem.append("<div id = '" + key  +"'>" + key + ": " + dt[key] + "</div>"));
-                            lastElem = $("#" + key);
-                        }
-                        
-                    }
-
-                    
-                }
+        function descrGet(descr){
+            var res = "";
+            path = app.items;
+            for(var i = 0; i < descr.length; i++){
+                path = path[descr[i]];
             }
+            if(typeof path == "object"){
+                path =  "";
+            }
+            return path;
         }
         
-        itemShow(data, "weather-container");
-        
+        function itemShow(dt, lvl, descr) {
+            var lastElem = $("#" + lvl);
+            var descrLocal = [];
+            for (var i = 0; i < descr.length; i++){
+                descrLocal.push(descr[i]);
+            }
+            for (var i = 0; i < Object.keys(dt).length; i++) {
+                var key = Object.keys(dt)[i];
+                descr.push(key);
+                if (typeof dt[key] == "object") {
+                    
+                    $(lastElem.append("<ul id = '" + key + "'>" + key +  "</ul>"));
 
-        //$("#weather-container").text(data);
+                    if (Array.isArray(dt[key])) {
+                        for (var ii = 0; ii < dt[key].length; ii++) {
+                            itemShow(dt[key][ii], key, descrLocal);
+                        }
+                    } else {
+                        
+                        itemShow(dt[key], key, descrLocal);
+                    }
+                } else {
+                    $(lastElem.append("<li id = '" + key + "'>" + descrGet(descrLocal) + "(" + key + "): " + dt[key] + "</li>"));
+                    }
+                }
+            }
+        itemShow(data, "weather-container", [] );
+        });
+    
+};
 
-        /*
-          {"coord":{"lon":139,"lat":35},
-          "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-          "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-          "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-          "wind":{"speed":7.31,"deg":187.002},
-          "rain":{"3h":0},
-          "clouds":{"all":92},
-          "dt":1369824698,
-          "id":1851632,
-          "name":"Shuzenji",
-          "cod":200}
-        */
-/*
-        $("#lat").text("lat:" + data.coord.lat);
-        $("#lon").text("lon:" + data.coord.lon);
-
-        //<div id = "sys">
-        $("#country").text(data.sys.country);
-        $("#sunrise").text(data.sys.sunrise);
-        $("#sunset").text(data.sys.sunset);
-        //        <div id = "weather">
-        $("#wid").text(data.weather[0].id);
-        $("#main").text(data.weather[0].main);
-        $("#description").text(data.weather[0].description);
-        $("#icon").text(data.weather[0].icon);
-        //    </div>
-        $("#temp").text(data.main.temp);
-        $("#humidity").text(data.main.humidity);
-        $("#pressure").text(data.main.pressure);
-        $("#temp_min").text(data.main.temp_min);
-        $("#temp_max").text(data.main.temp_max);
-        //    </div>
-        //    <div id = "wind">
-        $("#speed").text(data.wind.speed);
-        $("#deg").text(data.wind.deg);
-        //    </div>
-        //    <div id = "rain">
-        $("#3h").text(data.rain); //["3h"]);
-        //    </div>
-        //    <div id = "clouds">
-        $("#all").text(data.clouds.all);
-        //    </div>
-        $("#dt").text(data.dt);
-        $("#id").text(data.id);
-        $("#name").text(data.name);
-        $("#cod").text(data.cod);
-*/
-
-    })
-}
 
 $(document).ready(function () {
 
     var a = $("#weather-container");
     var b = $("#weather-fontainer");
-    
+
     app.weatherGet({
         "lon": 139,
         "lat": 35
